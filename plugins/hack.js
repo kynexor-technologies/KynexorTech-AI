5dc483c8969ff0fc2bf1f27eaecdc5a182060375
kynexor-technologies <kynexor.technologies@gmail.com>
2025-07-18 12:18:32 +0200

    Add files via upload

---
+++ plugins/hack.js
@@ -0,0 +1,33 @@
+const { smd, sleep } = require('../lib');
+
+smd({ cmdname: "hack", type: "fun", info: "hacking prank", filename: __filename }, async (citel) => {
+  const messages = [
+    "LuminaTechnologies Injecting Malware",
+    " █ 10%",
+    " █ █ 20%",
+    " █ █ █ 30%",
+    " █ █ █ █ 40%",
+    " █ █ █ █ █ 50%",
+    " █ █ █ █ █ █ 60%",
+    " █ █ █ █ █ █ █ 70%",
+    " █ █ █ █ █ █ █ █ 80%",
+    " █ █ █ █ █ █ █ █ █ 90%",
+    " █ █ █ █ █ █ █ █ █ █ 100%",
+    "System hyjacking on process.. \\n Conecting to Server error to find 404 ",
+    "Device successfully connected... \\n Riciving data...",
+    "Data hyjacked from divice 100% completed \\n killing all evidence killing all malwares...",
+    " HACKING COMPLETED ",
+    " SENDING LOG DOCUMENTS...",
+    " SUCCESSFULLY SENT DATA AND Connection disconnected",
+    "BACKLOGS CLEARED"
+    "POWERED BY LuminaTechnologies"
+    "By LuminaTech-V1®"
+  ];
+
+  let editedMessage;
+  for (const message of messages) {
+    editedMessage = await citel.send(editedMessage || message);
+    await sleep(1000);
+    editedMessage = await citel.edit(editedMessage, message);
+  }
+});
---
+++ plugins/intro.js
@@ -0,0 +1,94 @@
+/**
+//════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                            //
+//                                ＷＨＡＴＳＡＰＰ ＢＯＴ－ＭＤ ＢＥＴＡ                          //
+//                                                                                            // 
+//                                         Ｖ：2．5．0                                         // 
+//                                                                                            // 
+//                                                                                            // 
+//          ██████╗ ██╗███████╗████████╗███████╗██████╗     ███╗   ███╗██████╗                // 
+//         ██╔════╝ ██║██╔════╝╚══██╔══╝██╔════╝██╔══██╗    ████╗ ████║██╔══██╗               // 
+//         ██║  ███╗██║█████╗     ██║   █████╗  ██║  ██║    ██╔████╔██║██║  ██║               // 
+//         ██║   ██║██║██╔══╝     ██║   ██╔══╝  ██║  ██║    ██║╚██╔╝██║██║  ██║               // 
+//         ╚██████╔╝██║██║        ██║   ███████╗██████╔╝    ██║ ╚═╝ ██║██████╔╝               // 
+//          ╚═════╝ ╚═╝╚═╝        ╚═╝   ╚══════╝╚═════╝     ╚═╝     ╚═╝╚═════╝                //
+//                                                                                            //
+//                                                                                            //
+//                                                                                            //
+//                                                                                            //
+//════════════════════════════════════════════════════════════════════════════════════════════//
+*                                                                 
+  * @project_name : LuminaTech-V1 
+   * @author : LuminaTechnologies.
+   * @youtube : https://www.youtube.com
+   * @description : LuminaTech-V1 ,A Multi-functional whatsapp user bot.
+   * @version 2.5.0
+*
+* 
+   * Created By LuminaTechnologies.
+   * © 2025 LuminaTech-V1.
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Fork And Edit AS You Wish //
+
+const { smd, Config,smdBuffer,  prefix } = require('../lib')
+
+
+var surl = 'https://github.com/sircylee/LuminaTech-V1' // Source URL
+const number = '27643779064'
+var name = ' LuminaTech-V1'
+var body = '𝑇𝛩𝑈𝐶𝛨 𝛨𝛯𝑅𝛯'
+var image = 'https://files.catbox.moe/u6vije.jpg'
+let text = `╭═══ ━ ━ ━ ━ • ━ ━ ━ ━ ═══♡᭄
+│       「 LuminaTech-V1 by LuminaTechnologies  」
+│ Name      : Lumina
+│ Place     : South Africa, Johannesburg. 
+│ Gender    :  Female 
+│ Age          : 20
+│ education : 𝐁𝐒c IT 
+│ good vibes : 𝐒𝐓𝐀𝐘 𝐂𝐋𝐀𝐌
+│ Phone     : wa.me/27643779064
+│ Youtube   : youtube.com
+│ GitHub    : https://github.com/sircylee
+
+╰═══ ━ ━ ━ ━ • ━ ━ ━ ━ ═══♡᭄`
+
+
+
+
+
+ //---------------------------------------------------------------------------
+ smd({
+             pattern: "intro",
+             alias: ["lumina","lum"],
+             desc: "Show intro of user",
+             category: "fun",
+             filename: __filename,
+             use: '<group link.>',
+         },
+         async(message) => {
+    try{
+          let media ;try{ media = await smdBuffer(image) }catch{media = log0}
+           const q =await message.bot.fakeMessage("contact",{},name) 
+           let contextInfo = {...(await message.bot.contextInfo(name,body,media,1,surl, 2) )}
+           await message.send(text, {contextInfo : contextInfo },"suhail",  q )
+    }catch(e){ await message.error(`${e}\n\ncommand: intro`,e,false)}
+
+
+ })
